ef7b1c7f519bbf27ad80083ffd0e0f154a99367d
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-03-21 13:46:47 +0300

    archive link added
    (cherry picked from commit 871efe75d7f8d6c634ef4902a70d27835e64469c)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,10 @@ const config = {
                         label: 'Архив',
                         position: 'left',
                         items: [
-
+                            {
+                                label: 'Семестр 1',
+                                href: '/archive/semester/1',
+                            }
                         ],
                     },
                     // {
